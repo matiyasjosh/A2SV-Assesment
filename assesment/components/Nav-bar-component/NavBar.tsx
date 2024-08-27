f04a7c37c99540bf8544628c28307cf9811fe9f5
matiyasjosh <torbaw457@gmail.com>
2024-08-27 12:59:23 +0300

    modified:   assesment/app/page.tsx 	new file:   assesment/components/Nav-bar-component/NavBar.tsx 	new file:   assesment/components/ui/Button.tsx 	new file:   assesment/public/images/a2svLogo.jpeg

---
+++ assesment/components/Nav-bar-component/NavBar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Button from "../ui/Button";
+
+const NavBar = () => {
+  return (
+    <div className="flex my-10 mx-10 justify-between">
+      <Image src="/images/a2svLogo.jpeg" alt="a2sv logo" width={120} height={120} className="" />
+      
+      <div className="navLinks mt-3 flex gap-12 text-gray-700 text-[16px] font-medium">
+        <Link href={"/"} className="">Home</Link>
+        <Link href={"/"} className="">Teams</Link>
+        <Link href={"/"} className="">Success Stories</Link>
+        <Link href={"/"} className="">About Us</Link>
+        <Link href={"/blog"} className="">Blogs</Link>
+        <Link href={"/"} className="">Get Involved</Link>
+      </div>
+        <div className="buttons flex mt-1">
+            <button className="font-bold mr-7">Login</button>
+            <Button text="Donate" round="rounded-lg"/>
+        </div>
+
+    </div>
+  );
+};
+
+export default NavBar;
